4f7b6beaccf27e94dca04f256b91e006826baf4e
vignybrayan01 <89198968+vignybrayan01@users.noreply.github.com>
2024-07-07 20:52:26 +0200

    Add files via upload

---
+++ highScore.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let scoresBtn = document.querySelector("#view-high-scores");
+
+
+    function printHighscores() {
+
+        let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+
+        highscores.sort(function (a, b) {
+            return b.score - a.score;
+        });
+
+        highscores.forEach(score => {
+            let liTag = document.createElement("li");
+            liTag.textContent = score.name + " - " + score.score;
+            let olEl = document.getElementById("highscores");
+            olEl.appendChild(liTag);
+        });
+
+
+    }
+
+    // Clear previous scores when users click clear
+
+    function clearHighscores() {
+        window.localStorage.removeItem("highscores");
+        window.location.reload();
+    }
+    document.getElementById("clear").onclick = clearHighscores;
+
+    printHighscores();
+
+});
